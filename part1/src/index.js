d56bf6f2a6b4548d53801b7628b95f0c69ae0564
szaft <shaftxtreme@gmail.com>
2019-06-02 14:47:43 +0200

    part 1a

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,12 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Header = (prop) =>{
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    return(
+        <>
+            <h1>{prop.course}</h1>
+        </>
+    )
+}
+
+const Content = (prop) => {
+    return(
+        <>
+            <Part part={prop.p1} exercises={prop.e1}/>
+            <Part part={prop.p2} exercises={prop.e2}/>
+            <Part part={prop.p3} exercises={prop.e3}/>
+        </>
+    )
+}
+
+const Part = (prop) => {
+    return(
+        <>
+            <p>
+                {prop.part} {prop.exercises}
+            </p>
+        </>
+    )
+}
+
+const Total = (prop) => {
+    return(
+        <>
+            <p>
+                Number of exercises {prop.sum}
+            </p>
+        </>
+    )
+}
+
+
+const App = () => {
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+
+return(
+    <div>
+        <Header course={course}/>
+        <Content p1={part1} e1={exercises1}
+        p2={part2} e2={exercises2}
+        p3={part3} e3={exercises3}/>
+        <Total sum={exercises1+exercises2+exercises3}/>
+    </div>
+)
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
+
