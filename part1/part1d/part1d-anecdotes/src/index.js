3d6b1bd33d8ac3d36466fb2c8309f956205ee858
szaft <shaftxtreme@gmail.com>
2019-06-09 12:06:38 +0200

    votes, most votes

--- part1/part1d/part1d-anecdotes/src/index.js
+++ part1/part1d/part1d-anecdotes/src/index.js
@@ -7,25 +7,54 @@ const Button = ({text, handleClick}) => {
     )
 }
 
-const Anecdote = ({text}) => {
+const Anecdote = ({votes, text}) => {
     return (
+        <>
+        <h1>Anecdote of the day</h1>
         <div>{text}</div>
+        <VotesCount count={votes}/>
+        </>
+    )
+}
+
+const AnecdoteBest = ({votes, text}) => {
+    const maxIndex = votes.indexOf(Math.max(...votes))
+    return (
+        <>
+        <h1>Anecdote with the most votes</h1>
+        <div>{text[maxIndex]}</div>
+        <VotesCount count={votes[maxIndex]}/> 
+        </>
+    )
+}
+
+const VotesCount = ({count}) => {
+    return(
+        <p>has {count} votes</p>
     )
 }
 
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(6).fill(0))
   const setValue = (value, func) => () => func(value)
+  const incrementItem = (index, arr) => () => {
+    const list = arr.map((item, j) => j === index ? item+1 : item)
+    return list
+  }
   const randomNumber = (previous) => {
     let random = Math.floor(Math.random() * 6)
     return (random===previous ? randomNumber(random) : random)
   }
-  console.log(randomNumber(selected))
+  console.log('votes for position ',selected,': ',votes[selected])
+  console.log('state of array: ',votes)
   return (
     <div>
-        <Anecdote text={props.anecdotes[selected]}/>
+        <Anecdote votes={votes[selected]} text={props.anecdotes[selected]}/>
         <Button handleClick={setValue(randomNumber(selected), setSelected)} text='other anecdote'/>
+        <Button handleClick={setValue(incrementItem(selected, votes), setVotes)} text='vote'/>
+        <AnecdoteBest votes={votes} text={props.anecdotes}/>
     </div>
   )
 }
