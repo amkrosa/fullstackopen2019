8fe74b96d7dc31a88edb38246618e065a886af21
szaft <shaftxtreme@gmail.com>
2019-06-17 20:06:00 +0200

    part2b useReducer

--- part2/part2b/src/App.js
+++ part2/part2b/src/App.js
@@ -1,24 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
 import PersonForm from './components/PersonForm'
 import PhoneBook from './components/PhoneBook'
+import Filter from './components/Filter'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { id: 1,
-      name: 'Arto Hellas',
-      phone: '123123123' }
-  ]) 
 
-  const [ newName, setNewName ] = useState('')
-    const [ newNumber, setNewNumber ] = useState('')
-  
-  const handleNameChange = (event) => {
-      setNewName(event.target.value)
-  }
-  
-  const handleNumberChange = (event) =>{
-      setNewNumber(event.target.value)
-  }
+  const [ state, updateState ] = useReducer(
+    (state, newState) => ({...state, ...newState}),
+    {name: '', number: '', filter: '', persons: []}
+  )
+
+  const { name, number, filter, persons } = state
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -29,22 +21,27 @@ const App = () => {
     }
     persons.filter(person=>newName.toLowerCase()===person.name.toLowerCase()).length > 0
     ? alert(`${newName} already exists`)
-    : setPersons(persons.concat(newObject))
-    setNewName('')
-    setNewNumber('')
+    : updateState({ persons: persons.concat(newObject) })
+    updateState({ name: '', number: '' })
   }
 
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Filter
+        handleFilterChange={handleFilterChange}
+        newFilter={newFilter}
+        persons={persons}
+      />
+      <h2>Add new</h2>
       <PersonForm
-      persons={persons}
-      addPerson={addPerson}
-      newName={newName}
-      handleNameChange={handleNameChange}
-      newNumber={newNumber}
-      handleNumberChange={handleNumberChange}
+        persons={persons}
+        addPerson={addPerson}
+        name={name}
+        handleNameChange={e => updateState({ name: e.target.value })}
+        number={number}
+        handleNumberChange={e => updateState({ number: e.target.value })}
       />
       <h2>Numbers</h2>
       <PhoneBook persons={persons}/>
--- part2/part2b/src/components/PersonForm.js
+++ part2/part2b/src/components/PersonForm.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const PersonForm = ({addPerson, newName, handleNameChange, newNumber, handleNumberChange}) => {
+const PersonForm = ({addPerson, name, handleNameChange, number, handleNumberChange}) => {
     
     return(
     <div>
         <form onSubmit={addPerson}>
         <div>
-          name: <input type='text' value={newName} onChange={handleNameChange} required/>
+          name: <input type='text' value={name} onChange={handleNameChange} required/>
         </div>
         <div>
-          phone: <input type='number' value={newNumber} onChange={handleNumberChange} required/>
+          phone: <input type='number' value={number} onChange={handleNumberChange} required/>
         </div>
         <div>
           <button type="submit">add</button>
